e49435e3c19c95285ae92442673313f7d1043770
Naveen Kumar Gopi <naveen.gopi@imaginea.com>
2018-04-26 17:26:39 +0530

    reverting logout changes

--- src/main/java/com/pramati/ts/oauth/server/controller/LogoutController.java
+++ src/main/java/com/pramati/ts/oauth/server/controller/LogoutController.java
@@ -15,34 +15,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @FrameworkEndpoint
 public class LogoutController {
 	
-	
-	private AuthorizationServerEndpointsConfigurer endpoints = new AuthorizationServerEndpointsConfigurer();
     
-    @Bean
-	public AuthorizationServerTokenServices defaultAuthorizationServerTokenServices() {
-		return endpoints.getDefaultAuthorizationServerTokenServices();
-	}
 
-	public AuthorizationServerEndpointsConfigurer getEndpointsConfigurer() {
-		if (!endpoints.isTokenServicesOverride()) {
-			endpoints.tokenServices(defaultAuthorizationServerTokenServices());
-		}
-		return endpoints;
-	}
-	
-	@Bean
-	public ConsumerTokenServices consumerTokenServices() throws Exception {
-		return getEndpointsConfigurer().getConsumerTokenServices();
-	}
-    
- 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/oauth/token")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/oauth/revokeToken")
     @ResponseBody
     public void revokeToken(HttpServletRequest request) throws Exception {
         String authorization = request.getHeader("Authorization");
         if (authorization != null && authorization.contains("Bearer")){
             String tokenId = authorization.substring("Bearer".length()+1);
-            consumerTokenServices().revokeToken(tokenId);
+            //consumerTokenServices().revokeToken(tokenId);
         }
     }
 }
